3878e8b3afdb45b3ccb23fecf6b423112b45f76a
sink666 <er.basilisk@gmail.com>
2020-12-07 02:20:01 +0000

    refactor

--- dland.c
+++ dland.c
@@ -9,12 +9,8 @@ int r_slen() {
     return (rand() % (30 - 7)) + 7;
 }
 
-int n_pgraphs() {
-    return (rand() % 7) + 1;
-}
-
-int drand(int x) {
-    return (rand() % x);
+int drand(int n1, int n2) {
+    return (rand() % n1) + n2;
 }
 
 void getnewword(FILE *fp, char* word, int line)
@@ -42,12 +38,12 @@ void writesentence(FILE* ifp, FILE* ofp)
     char word[30]; /* boy makes up some long ass words */
 
     for(int j = r_slen(); j != 0; j--) {
-	getnewword(ifp, word, drand(MAXWORDS));
+	getnewword(ifp, word, drand(MAXWORDS, 0));
 	fputs(word, ofp);
 
-	if(drand(2)) { /* coin flip with a safety */
-	    if(drand(9) < 2)
-		fputc(punctuation[drand(5)], ofp);
+	if(drand(2, 0)) { /* coin flip with a safety */
+	    if(drand(9, 0) < 2)
+		fputc(punctuation[drand(5, 0)], ofp);
 	}
 
 	if(j > 1)
@@ -71,7 +67,7 @@ int main(void)
 
     fputs(p_head, outp_fp);
     fputs(opener, outp_fp);
-    for(int i = n_pgraphs(); i != 0; i--) {
+    for(int i = n_pgraphs(7, 1); i != 0; i--) {
 	writesentence(dict_fp, outp_fp);
 	fputc('.', outp_fp);
 	fputs(spread, outp_fp);
